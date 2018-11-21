852edca98bc679622de206b8951c92f2fd1c1fe2
黄翔 <hxhomemail@163.com>
2018-11-21 15:44:04 +0800

    Initial commit

---
+++ src/main/java/io/appium/AppiumDemo.java
@@ -0,0 +1,51 @@
+package io.appium;
+
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @ClassName AppiumDemo
+ * @Description IOS-AppiumDemo
+ * @Author huangxiang
+ * @Date 2018/11/21 3:28 PM
+ * @Version 1.0
+ **/
+public class AppiumDemo {
+
+    private IOSDriver driver;
+
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "ios");
+        capabilities.setCapability("platformVersion", "12.1");
+        capabilities.setCapability("deviceName", "iPhone XS Max");
+        capabilities.setCapability("app", "/Users/huangxiang/Downloads/TestApp.app");
+        capabilities.setCapability("noReset", true);
+
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+
+    @Test
+    public void testDemo()  {
+        throw new UnsupportedOperationException();
+
+    }
+
+
+}
